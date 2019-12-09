0abc28981c9ab895201a7dd44bf1fbea4e9e8505
martinajir <martina.jireckova@gmail.com>
2019-12-09 17:19:50 +0000

    removed unnecessary lines

--- gameOfLife/src/gameOfLife/Game.java
+++ gameOfLife/src/gameOfLife/Game.java
@@ -1,14 +1,9 @@
 package gameOfLife;
 
 import gameOfLife.gui.GameWindow;
-import gameOfLife.rules.RuleSet;
-import gameOfLife.structures.Grid;
 
 public class Game {
     public static final int STARTER_CELLS_COUNT = 15;
-
-    private Grid gameGrid;
-    private RuleSet ruleSet;
 
     public static void main(String[] args){
         new GameWindow(5,5);
